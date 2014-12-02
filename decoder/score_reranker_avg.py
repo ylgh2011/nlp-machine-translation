7376c063bf60038432b555e9c570eb9f0a04333e
Fan (Michael) Gao <gaofang@sfu.ca>
2014-12-01 22:58:36 -0800

    Add score_rerank_ave to calculate average bleu
    Former-commit-id: 2e6cba85362e1100e8cbbe8d1238f328de799c0c

---
+++ decoder/score_reranker_avg.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import optparse
+import sys
+import bleu
+
+def main(opts, sysstdin):
+	system = [line.strip().split() for line in sysstdin]
+	score = 0.0
+	ref = [line.strip().split() for line in open(opts.en0)]
+	score += cal_store(ref, system)
+	ref = [line.strip().split() for line in open(opts.en1)]
+	score += cal_store(ref, system)
+	ref = [line.strip().split() for line in open(opts.en2)]
+	score += cal_store(ref, system)
+	ref = [line.strip().split() for line in open(opts.en3)]
+	score += cal_store(ref, system)
+	return score/4
+
+
+def cal_store(ref, system):
+	stats = [0 for i in xrange(10)]
+	for (r,s) in zip(ref, system):
+		stats = [sum(scores) for scores in zip(stats, bleu.bleu_stats(s,r))]
+	return bleu.bleu(stats)
+
+if __name__ == "__main__":
+	optparser = optparse.OptionParser()
+	# ptparser.add_option("--ref", dest="en", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="target language references for learning how to rank the n-best list")
+	optparser.add_option("--en0", dest="en0", default="/usr/shared/CMPT/nlp-class/project/test/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
+	optparser.add_option("--en1", dest="en1", default="/usr/shared/CMPT/nlp-class/project/test/all.cn-en.en1", help="target language references for learning how to rank the n-best list")
+	optparser.add_option("--en2", dest="en2", default="/usr/shared/CMPT/nlp-class/project/test/all.cn-en.en2", help="target language references for learning how to rank the n-best list")
+	optparser.add_option("--en3", dest="en3", default="/usr/shared/CMPT/nlp-class/project/test/all.cn-en.en3", help="target language references for learning how to rank the n-best list")
+	(opts,_) = optparser.parse_args()
+	print main(opts, sys.stdin)
+
