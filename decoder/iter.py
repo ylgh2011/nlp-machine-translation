52a78759dbc4e8d782e6b63738719864762c5a57
Louis Ye <lvyuy@asb9804u-a04.csil.sfu.ca>
2014-12-01 20:54:10 -0800

    iter.py
    Former-commit-id: 21b74b19215ca240c845fe9d14294361500c03ff

--- decoder/iter.py
+++ decoder/iter.py
@@ -3,6 +3,7 @@ import optparse
 import sys
 import copy
 from collections import namedtuple
+from score-reranker.py
 
 import beam
 import cal_weight
@@ -18,26 +19,32 @@ optparser.add_option("-i", "--iter", dest="iter", default=5, type="int", help="N
 
 ########################################################################################################################################
 ## Parameters for decoder part
+# optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="File containing sentences to translate (default=data/input)")
 optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.cn", help="File containing sentences to translate (default=data/input)")
+# optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/small/train.cn", help="File containing sentences to translate (default=data/input)")
+# optparser.add_option("--input", dest="input", default="../segmenter/train.cn", help="File containing sentences to translate (default=data/input)")
 
 # optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/toy/phrase-table/phrase_table.out", help="File containing translation model (default=data/tm)")
-# optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/small/phrase-table/moses/phrase-table.gz", help="File containing translation model (default=data/tm)")
 optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/large/phrase-table/dev-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
+# optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/large/phrase-table/test-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
 
-optparser.add_option("--language-model", dest="lm", default="/usr/shared/CMPT/nlp-class/project/lm/en.gigaword.3g.filtered.train_dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
 # optparser.add_option("--language-model", dest="lm", default="/usr/shared/CMPT/nlp-class/project/lm/en.tiny.3g.arpa", help="File containing ARPA-format language model (default=data/lm)")
+optparser.add_option("--language-model", dest="lm", default="/usr/shared/CMPT/nlp-class/project/lm/en.gigaword.3g.filtered.train_dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
 
-optparser.add_option("--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
-optparser.add_option("--translations-per-phrase", dest="k", default=20, type="int", help="Limit on number of translations to consider per phrase (default=1)")
+optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
+optparser.add_option("-k", "--translations-per-phrase", dest="k", default=20, type="int", help="Limit on number of translations to consider per phrase (default=1)")
 optparser.add_option("--heap-size", dest="s", default=1000, type="int", help="Maximum heap size (default=1)")
 optparser.add_option("--disorder", dest="disord", default=5, type="int", help="Disorder limit (default=6)")
-optparser.add_option("--beam width", dest="bwidth", default=1.0,  help="beamwidth")
+optparser.add_option("--beam width", dest="bwidth", default=2,  help="beamwidth")
 optparser.add_option("--mute", dest="mute", default=0, type="int", help="mute the output")
 optparser.add_option("--nbest", dest="nbest", default=100, type="int", help="print out nbest results")
 
 ########################################################################################################################################
 ## Parameters for reranker part
-optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
+# optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="target language references for learning how to rank the n-best list")
+# optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
+optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/small/train.en", help="target language references for learning how to rank the n-best list")
+
 optparser.add_option("--epo", dest="epo", type="int",default=5, help="number of epochs for perceptron training (default 5)")
 optparser.add_option("--eta", dest="eta", type="float", default=0.1, help="perceptron learning rate (default 0.1)")
 optparser.add_option("--xi", dest="xi", type="int", default=100, help="training data generated from the samples tau (default 100)")
@@ -62,7 +69,8 @@ for word in set(sum(french,())):
     if (word,) not in tm:
         tm[(word,)] = [models.phrase(word, [0.0, 0.0, 0.0, 0.0])]
 
-ibm_t = library.init('./data/ibm2.t.ds.gz')
+# ibm_t = library.init('./data/ibm2.t.ds.gz')
+ibm_t = {}
 
 
 ########################################################################################################################################
@@ -81,16 +89,24 @@ sys.stderr.write("\n")
 ## start doing iteration between decoder and reranker
 w = [1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
 nbest_sentences = []
+current_bleu_score = 0
 for i in range(opts.iter):
     sys.stderr.write("Iteration %d\n" % i)
     nbest_sentences = beam.main(opts, w, tm, lm, french, ibm_t)
     w_str = cal_weight.main(opts, references, nbest_sentences)
+
     w = [float(item) for item in w_str.split('\n')]
     sys.stderr.write("w = " + str(w) + '\n')
 
-(score_list, translation_list) = rerank.main(w, nbest_sentences)
-for (score, translation) in zip(score_list, translation_list):
-    sys.stderr.write(str(score) + '\n')
-    print translation
+
+
+
+print w
+# (score_list, translation_list) = rerank.main(w, nbest_sentences)
+# counter = 0
+# for (score, translation) in zip(score_list, translation_list):
+#     counter += 1
+    # sys.stderr.write('______________ # ' + str(counter) + ' score: ' + str(score) + '\n')
+    # print translation
 
 # print '\n'.join([str(item) for item in w])
