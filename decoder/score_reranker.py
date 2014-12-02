73cb376b97043a744d3f94d1564126cb9425dcb0
Fan (Michael) Gao <gaofang@sfu.ca>
2014-12-01 21:55:47 -0800

    Modify Score Reranker
    Former-commit-id: 1e46f6e3957e30299d2beabe659518502da6f026

--- decoder/score_reranker.py
+++ decoder/score_reranker.py
@@ -16,6 +16,7 @@ def main(opts, sysstdin):
 if __name__ == "__main__":
 	optparser = optparse.OptionParser()
 	# ptparser.add_option("--ref", dest="en", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="target language references for learning how to rank the n-best list")
-	optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
+	optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/test/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
 	(opts,_) = optparser.parse_args()
-	main(opts, sys.stdin)
+	print main(opts, sys.stdin)
+
