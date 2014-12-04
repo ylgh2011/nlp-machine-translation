c99083e36ed57fd0dbe8a1149d5077d6a45c652e
Fan (Michael) Gao <gaofang@sfu.ca>
2014-12-04 02:00:56 -0800

    Modify beam and iter for only five featrues
    Former-commit-id: 0f77a98cc058a246da92b2f4c9b54629b4f958ee

---
+++ decoder/beam_original.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import optparse
+import sys
+import models
+import copy
+from collections import namedtuple
+from models import getDotProduct
+
+from library import read_ds_from_file
+from library import write_ds_to_file
+from library import init
+from library import t_f_given_e
+from library import q_j_given_i_l_m
+from library import ibm_model_1_score
+from library import ibm_model_1_w_score
+
+def bitmap(sequence):
+    """ Generate a coverage bitmap for a sequence of indexes """
+    return reduce(lambda x,y: x|y, map(lambda i: long('1'+'0'*i,2), sequence), 0)
+
+def onbits(b):
+    """ Count number of on bits in a bitmap """
+    return 0 if b==0 else (1 if b&1==1 else 0) + onbits(b>>1)
+
+def prefix1bits(b):
+    """ Count number of bits encountered before first 0 """
+    return 0 if b&1==0 else 1+prefix1bits(b>>1)
+
+def last1bit(b):
+    """ Return index of highest order bit that is on """
+    return 0 if b==0 else 1+last1bit(b>>1)
+
+# def stateeq(state1, state2):
+#     return (state1.lm_state == state2.lm_state) and (state1.end == state2.end) and (state1.coverage == state2.coverage)
+hypothesis = namedtuple("hypothesis", "lm_state, logprob, coverage, end, predecessor, phrase, distortionPenalty")
+
+def main(opts, w, tm, lm, french, ibm_t):
+    # tm should translate unknown words as-is with probability 1
+    # if w is None:
+    #     w = [1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+
+    nbest_output = []
+    total_prob = 0
+    if opts.mute == 0:
+        sys.stderr.write("Start decoding %s ...\n" % (opts.input,))
+    for idx,f in enumerate(french):
+        if opts.mute == 0 and idx % 500 == 0:
+            sys.stderr.write("Decoding sentence #%s ...\n" % (str(idx)))
+        initial_hypothesis = hypothesis(lm.begin(), 0.0, 0, 0, None, None, None)
+        heaps = [{} for _ in f] + [{}]
+        heaps[0][lm.begin(), 0, 0] = initial_hypothesis
+        for i, heap in enumerate(heaps[:-1]):
+            # maintain beam heap
+            front_item = sorted(heap.itervalues(), key=lambda h: -h.logprob)[0]
+
+            for h in sorted(heap.itervalues(),key=lambda h: -h.logprob)[:opts.s]: # prune
+                if h.logprob < front_item.logprob - float(opts.bwidth): continue
+
+                fopen = prefix1bits(h.coverage)
+                for j in xrange(fopen,min(fopen+1+opts.disord, len(f)+1)):
+                    for k in xrange(j+1, len(f)+1):
+                        if f[j:k] in tm:
+                            if (h.coverage & bitmap(range(j, k))) == 0:
+                                for phrase in tm[f[j:k]]:
+                                    lm_prob = 0
+                                    lm_state = h.lm_state
+                                    for word in phrase.english.split():
+                                        (lm_state, prob) = lm.score(lm_state, word)
+                                        lm_prob += prob
+                                    lm_prob += lm.end(lm_state) if k == len(f) else 0.0
+                                    coverage = h.coverage | bitmap(range(j, k))
+                                    # logprob = h.logprob + lm_prob*w[0] + getDotProduct(phrase.several_logprob, w[2:6]) + abs(h.end+1-j)*w[1] + ibm_model_1_w_score(ibm_t, f, phrase.english)*w[6]
+                                    logprob  = h.logprob
+                                    logprob += lm_prob*w[0]
+                                    logprob += getDotProduct(phrase.several_logprob, w[1:5])
+                                    #logprob += opts.diseta*abs(h.end+1-j)*w[1]
+                                    #logprob += ibm_model_1_w_score(ibm_t, f, phrase.english)*w[6]
+
+                                    new_hypothesis = hypothesis(lm_state, logprob, coverage, k, h, phrase, abs(h.end + 1 - j))
+
+                                    # add to heap
+                                    num = onbits(coverage)
+                                    if (lm_state, coverage, k) not in heaps[num] or new_hypothesis.logprob > heaps[num][lm_state, coverage, k].logprob:
+                                        heaps[num][lm_state, coverage, k] = new_hypothesis
+
+
+        winners = sorted(heaps[-1].itervalues(), key=lambda h: h.logprob)[0:opts.nbest]
+
+        def get_lm_logprob(test_list):
+            stance = []
+            for i in test_list:
+                stance += (i.split())
+            stance = tuple(stance)
+            lm_state = (stance[0],)
+            score = 0.0
+            for word in stance:
+                (lm_state, word_score) = lm.score(lm_state, word)
+                score += word_score
+            return score
+        def get_list_and_features(h):
+            lst = [];
+            features = [0, 0, 0, 0, 0]
+            current_h = h;
+            while current_h.phrase is not None:
+                # print current_h
+                lst.append(current_h.phrase.english);
+                #features[1] += current_h.distortionPenalty              # distortion penaulty
+                features[1] += current_h.phrase.several_logprob[0]      # translation feature 1
+                features[2] += current_h.phrase.several_logprob[1]      # translation feature 2
+                features[3] += current_h.phrase.several_logprob[2]      # translation feature 3
+                features[4] += current_h.phrase.several_logprob[3]      # translation feature 4
+                current_h = current_h.predecessor
+            lst.reverse()
+            features[0] = get_lm_logprob(lst)                           # language model score
+            #features[6] = ibm_model_1_score(ibm_t, f, lst)
+            return (lst, features)
+
+        for win in winners:
+            s = str(idx) + " ||| "
+            (lst, features) = get_list_and_features(win)
+            for word in lst:
+                s += word + ' '
+            s += '||| '
+            for fea in features:
+                s += str(fea) + ' '
+            nbest_output.append(s)
+
+    # log_file = open('./log.txt','wb')
+    # log_file.write(str(nbest_output))
+
+    return nbest_output
+
+# if __name__ == "__main__":
+#     # main()
+
---
+++ decoder/iter_original.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+import optparse
+import sys
+import copy
+from collections import namedtuple
+
+
+import beam_original
+import cal_weight
+import models
+import library
+import rerank
+import score_reranker_avg
+
+optparser = optparse.OptionParser()
+
+########################################################################################################################################
+## Parameters for iter
+optparser.add_option("-i", "--iter", dest="iter", default=5, type="int", help="Number of iterations between decoder and reranker")
+
+########################################################################################################################################
+## Parameters for decoder part
+# optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="File containing sentences to translate (default=data/input)")
+optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.cn", help="File containing sentences to translate (default=data/input)")
+# optparser.add_option("--input", dest="input", default="/usr/shared/CMPT/nlp-class/project/small/train.cn", help="File containing sentences to translate (default=data/input)")
+# optparser.add_option("--input", dest="input", default="../segmenter/train.cn", help="File containing sentences to translate (default=data/input)")
+
+#optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/toy/phrase-table/phrase_table.out", help="File containing translation model (default=data/tm)")
+optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/large/phrase-table/dev-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
+# optparser.add_option("--translation-model", dest="tm", default="/usr/shared/CMPT/nlp-class/project/large/phrase-table/test-filtered/rules_cnt.final.out", help="File containing translation model (default=data/tm)")
+
+#optparser.add_option("--language-model", dest="lm", default="/usr/shared/CMPT/nlp-class/project/lm/en.tiny.3g.arpa", help="File containing ARPA-format language model (default=data/lm)")
+optparser.add_option("--language-model", dest="lm", default="/usr/shared/CMPT/nlp-class/project/lm/en.gigaword.3g.filtered.train_dev_test.arpa.gz", help="File containing ARPA-format language model (default=data/lm)")
+
+optparser.add_option("-n", "--num_sentences", dest="num_sents", default=sys.maxint, type="int", help="Number of sentences to decode (default=no limit)")
+optparser.add_option("-k", "--translations-per-phrase", dest="k", default=10, type="int", help="Limit on number of translations to consider per phrase (default=1)")
+optparser.add_option("--heap-size", dest="s", default=100, type="int", help="Maximum heap size (default=1)")
+optparser.add_option("--disorder", dest="disord", default=3, type="int", help="Disorder limit (default=3)")
+optparser.add_option("--diseta", dest="diseta", type="float", default=0.1, help="perceptron learning rate (default 0.1)")
+optparser.add_option("--beam width", dest="bwidth", default=0.5,  help="beamwidth")
+optparser.add_option("--mute", dest="mute", default=0, type="int", help="mute the output")
+optparser.add_option("--nbest", dest="nbest", default=100, type="int", help="print out nbest results")
+
+########################################################################################################################################
+## Parameters for reranker part
+# optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/toy/train.cn", help="target language references for learning how to rank the n-best list")
+# optparser.add_option("--en", dest="en", default="/usr/shared/CMPT/nlp-class/project/small/train.en", help="target language references for learning how to rank the n-best list")
+
+optparser.add_option("--en0", dest="en0", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en0", help="target language references for learning how to rank the n-best list")
+optparser.add_option("--en1", dest="en1", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en1", help="target language references for learning how to rank the n-best list")
+optparser.add_option("--en2", dest="en2", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en2", help="target language references for learning how to rank the n-best list")
+optparser.add_option("--en3", dest="en3", default="/usr/shared/CMPT/nlp-class/project/dev/all.cn-en.en3", help="target language references for learning how to rank the n-best list")
+
+optparser.add_option("--epo", dest="epo", type="int", default=5, help="number of epochs for perceptron training (default 5)")
+optparser.add_option("--eta", dest="eta", type="float", default=0.1, help="perceptron learning rate (default 0.1)")
+optparser.add_option("--xi", dest="xi", type="int", default=100, help="training data generated from the samples tau (default 100)")
+optparser.add_option("--alpha", dest="alpha", type="float", default=0.1, help="sampler acceptance cutoff (default 0.1)")
+optparser.add_option("--tau", dest="tau", type="int", default=5000, help="samples generated from n-best list per input sentence (default 5000)")
+
+########################################################################################################################################
+## opts
+opts = optparser.parse_args()[0]
+
+
+
+
+########################################################################################################################################
+## init for decoder part
+tm = models.TM(opts.tm, opts.k, opts.mute)
+lm = models.LM(opts.lm, opts.mute)
+french = [tuple(line.strip().split()) for line in open(opts.input).readlines()[:opts.num_sents]]
+bound_width = float(opts.bwidth)
+
+for word in set(sum(french,())):
+    if (word,) not in tm:
+        tm[(word,)] = [models.phrase(word, [0.0, 0.0, 0.0, 0.0])]
+
+ibm_t = {}
+#ibm_t = library.init('./data/ibm.t.gz')
+
+
+########################################################################################################################################
+## init for reranker part
+references = [[], [], [], []]
+sys.stderr.write("Reading English Sentences ... \n")
+def readReference(ref_fileName):
+    ref = []
+    for i, line in enumerate(open(ref_fileName)):
+        # Initialize references to correct english sentences
+        ref.append(line)
+        if i%1000 == 0:
+            sys.stderr.write(".")
+    sys.stderr.write("\n")
+    return ref
+
+
+references[0] = readReference(opts.en0)
+references[1] = readReference(opts.en1)
+references[2] = readReference(opts.en2)
+references[3] = readReference(opts.en3)
+
+########################################################################################################################################
+## start doing iteration between decoder and reranker
+# w = [1.0, -0.01, 1.0, 1.0, 1.0, 1.0, 1.0]
+w = [0.2, 0.2, 0.2, 0.2, 0.2]
+nbest_sentences_0 = beam.main(opts, w, tm, lm, french, ibm_t)
+(score_list, translation_list) = rerank.main(w, nbest_sentences_0)
+best_bleu_score = score_reranker_avg.main(opts, translation_list)
+print "w = " + str(w) +  ", score before iteration: " + str(best_bleu_score)
+
+for i in range(opts.iter):
+    sys.stderr.write("Iteration %d\n" % i)
+    # beam decode and output nbest file
+    nbest_sentences = beam.main(opts, w, tm, lm, french, ibm_t)
+
+    # calculate weight and output the result weight
+    new_w_str = cal_weight.main(opts, references, nbest_sentences, w)
+    new_w = [float(item) for item in new_w_str.split('\n')]
+
+    # rerank using the output weight 
+    (score_list, translation_list) = rerank.main(new_w, nbest_sentences)
+    
+    # calculate the BLEU score for the test set
+    new_bleu_score = score_reranker_avg.main(opts, translation_list)
+    sys.stderr.write("new_w = " + str(new_w) + ", new_bleu_score = " + str(new_bleu_score) + '\n')
+    if best_bleu_score < new_bleu_score:
+        best_bleu_score = new_bleu_score
+        w = new_w
+    else:
+        break
+
+
+
+
+
+
+for item in w:
+    print item
+
+
+# (score_list, translation_list) = rerank.main(w, nbest_sentences)
+# counter = 0
+# for (score, translation) in zip(score_list, translation_list):
+#     counter += 1
+    # sys.stderr.write('______________ # ' + str(counter) + ' score: ' + str(score) + '\n')
+    # print translation
+
+# print '\n'.join([str(item) for item in w])
+
+
+reload(beam)
+reload(cal_weight)
+reload(models)
+reload(library)
+reload(rerank)
+reload(score_reranker_avg)
